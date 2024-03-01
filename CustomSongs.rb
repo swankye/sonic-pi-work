6385050d28b8f673e4b4bf76e384436f726b7826
swankye <160068348+swankye@users.noreply.github.com>
2024-03-01 10:29:12 -0800

    Update CustomSongs.rb

--- CustomSongs.rb
+++ CustomSongs.rb
@@ -1,45 +1,42 @@
+sing = "C:/Users/tochtli_moreno-quant/Downloads/Asleep But Vocals Only.mp3"
+vocals  = "C:/Users/tochtli_moreno-quant/Documents/Audacity/corus.mp3"
+
+
+
+sample sing
+sleep 8
+sample vocals
 #mesure one
-use_bpm 75
-play :e5
-sleep 1
-play :a4
-sleep 1
-play :c5
-sleep 0.75
-play :d5
-sleep 0.25
-#measure two
-play :e5
-sleep 1
-play :b4
-sleep 1
-play :b4
-sleep 0.75
-play :d5
-sleep 0.25
-#measure three
-play :e5
-sleep 1
-play :a4
-sleep 1
-play :c5
-sleep 0.5
-play :a4
-sleep 0.5
-#measure four
-play :a4
-sleep 1.5
-play :r
-sleep 1.5
-
-
-#measure five
-#play :f4
-#sleep 0.5
-#play :e4
-#sleep 0.5
-#play :d4
-#sleep 0.5
-#play :c4
-#sleep 1.5
-#play :c4
+live_loop :beat do
+  use_bpm 77
+  play :e5
+  sleep 1
+  play :a4
+  sleep 1
+  play :c5
+  sleep 0.75
+  play :d5
+  sleep 0.25
+  #measure two
+  play :e5
+  sleep 1
+  play :b4
+  sleep 1
+  play :b4
+  sleep 0.75
+  play :d5
+  sleep 0.25
+  #measure three
+  play :e5
+  sleep 1
+  play :a4
+  sleep 1
+  play :c5
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  #measure four
+  play :a4
+  sleep 1.5
+  play :r
+end
