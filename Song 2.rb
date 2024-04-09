dc6b267e36db3a655347df11d686755ad2f352e3
swankye <160068348+swankye@users.noreply.github.com>
2024-04-09 15:40:39 -0700

    Song 2.rb
    4/9/24

---
+++ Song 2.rb
@@ -0,0 +1,46 @@
+#song 2
+use_synth :saw
+play :gs3, sustain: 1.5
+play :as3, sustain: 1.5
+play :cs4, sustain: 1.5
+play :f4, sustain: 1.5
+sleep 2.5
+play :gs3, sustain: 1.5
+play :cs4, sustain: 1.5
+play :ds4, sustain: 1.5
+play :f4, sustain: 1.5
+sleep 2.5
+play :gs3, sustain: 1.5
+play :as3, sustain: 1.5
+play :cs4, sustain: 1.5
+play :f4, sustain: 1.5
+sleep 2.5
+play :as5,sustain: 1
+play :as4,sustain: 1
+sleep 2
+play :f5,sustain: 1
+play :f4,sustain: 1
+sleep 2
+play :gs5,sustain: 1
+play :gs4,sustain: 1
+sleep 2
+play :gs4
+play :f4,sustain: 1.5
+play :as3,sustain: 1
+sleep 1
+play :gs4,sustain: 0.5
+sleep 0.5 #sine wave melody
+play :f4,sustain: 1.5
+#play :as3,sustain: 1
+play :ds4 ,sustain: 1.5
+sleep 1.5
+play :as4,sustain: 2
+sleep 0.5
+play :gs4 ,sustain: 1.5
+play :f4,sustain: 1.5
+sleep 0.5
+play :f4
+sleep 1
+play :cs4,sustain: 1.5
+sleep 0.5
+play :gs5,sustain: 1
